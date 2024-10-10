7ca72609ec7c29c0e37a96a730723fc7cca27569
merzlovgleb <merzlovgleb@gmail.com>
2024-10-10 19:56:05 +0300

    ДЗ 06.10

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 234;
+        int c = 7;
+
+        System.out.println(a);
+        System.out.println(c);
+
+        int div = a / c;
+        System.out.println(div);
+    }
+}
